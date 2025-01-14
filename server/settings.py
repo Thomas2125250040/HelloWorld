f375043f94d48dcc5886ba43856cc1db1f87ed74
Thomas2125250040 <thomassetiawan13@mhs.mdp.ac.id>
2025-01-15 01:40:19 +0700

    adding greet

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o95_udbf9=u=hswz0dmbkb1yr4e71o@c$u$^*l%%2200ir66)6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.43.154']
+ALLOWED_HOSTS = ['192.168.43.154', '127.0.0.1']
 
 
 # Application definition
